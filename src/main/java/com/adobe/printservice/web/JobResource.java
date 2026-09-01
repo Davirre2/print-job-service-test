881122375ea5b49d41aeb2df3a6fb2257b405c3c
Davit Mas <71844784+Davirre2@users.noreply.github.com>
2026-09-02 00:08:59 +0200

    Added GetByResult

--- src/main/java/com/adobe/printservice/web/JobResource.java
+++ src/main/java/com/adobe/printservice/web/JobResource.java
@@ -3,9 +3,12 @@ package com.adobe.printservice.web;
 import com.adobe.printservice.model.Job;
 import com.adobe.printservice.model.JobStatus;
 import com.adobe.printservice.service.JobService;
+import com.adobe.printservice.service.JobWorker;
 import com.adobe.printservice.web.dto.JobCreateRequest;
 import com.adobe.printservice.web.dto.JobResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +28,8 @@ import java.util.Optional;
 public class JobResource {
 
     private final JobService jobService;
+    private static final Logger log = LoggerFactory.getLogger(JobWorker.class);
+
 
     public JobResource(JobService jobService) {
         this.jobService = jobService;
@@ -47,5 +53,25 @@ public class JobResource {
         return jobService.listJobs(Optional.ofNullable(status)).stream()
                 .map(JobResponse::from)
                 .toList();
+    }
+
+    @GetMapping("/{id}/result")
+    public ResponseEntity<String> getJobResult(@PathVariable String id) {
+        Job job = jobService.getJob(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Job not found"));
+        if (job.getStatus() == JobStatus.FAILED) {
+            throw new IllegalArgumentException(
+                    "Job processing failed"
+            );
+        }
+
+        if (job.getStatus() != JobStatus.DONE) {
+            throw new IllegalArgumentException(
+                    "Job processing not finished yet"
+            );
+        }
+
+        return ResponseEntity.ok(job.getResultContent());
+
     }
 }
